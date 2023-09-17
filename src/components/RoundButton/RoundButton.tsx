609abe360802ec616f9e7194399b14db0efeee3f
Muhammd Yasir <shahzadyasir77@gmail.com>
2023-09-17 15:41:46 +0500

    notification and round button css modules

--- src/components/RoundButton/RoundButton.tsx
+++ src/components/RoundButton/RoundButton.tsx
@@ -1,18 +1,23 @@
+// RoundButton.tsx
 import React from 'react';
+import styles from './RoundButton.module.css'; // Import CSS module
 import { RoundButtonProps } from './RoundButton.types';
-import './RoundButton.css';
 
 const RoundButton = ({ size, btnType, selected, disabled = false, className, ...props }: RoundButtonProps) => {
   return (
-    <button disabled={disabled} className={['btn__round', `btn__round__${size}`, className].join(' ')} {...props}>
+    <button
+      disabled={disabled}
+      className={[styles.btn__round, styles[`btn__round__${size}`], className].join(' ')}
+      {...props}
+    >
       <div
         className={[
-          'btn__round__icon',
-          `${
-            selected
-              ? `btn__round--selected--${btnType}`
-              : `${disabled ? `btn__round--disabled--${btnType}` : `btn__round--${btnType}`}`
-          }`,
+          styles.btn__round__icon,
+          selected
+            ? styles[`btn__round--selected--${btnType}`]
+            : disabled
+            ? styles[`btn__round--disabled--${btnType}`]
+            : styles[`btn__round--${btnType}`],
         ].join(' ')}
       />
     </button>
