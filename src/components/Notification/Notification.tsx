6db9f76a466eeb82a41b77339d3a2db9bac95b4f
Muhammd Yasir <shahzadyasir77@gmail.com>
2023-09-17 15:29:28 +0500

    css modules notification component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,11 +1,12 @@
+// Notification.tsx
 import React from 'react';
+import styles from './Notification.module.css'; // Import CSS module
 import { NotificationProps } from './Notification.types';
-import './Notification.css';
 
 const Notification = ({ color, counter, mark }: NotificationProps) => {
   return (
-    <div className={['notification', `notification--${color}`].join(' ')}>
-      <div className="text">{mark ? '!' : counter}</div>
+    <div className={[styles.notification, styles[`notification--${color}`]].join(' ')}>
+      <div className={styles.text}>{mark ? '!' : counter}</div>
     </div>
   );
 };
